d49960a1a2a1ef9234b2b2a8c2cac77e26072428
Oleg Mihailik <mihailik@gmail.com>
2025-09-05 10:45:13 +0100

    Whitespace tweaks to HTML/XML tests.

--- parser/tests/5-html-xml.test.ts
+++ parser/tests/5-html-xml.test.ts
@@ -3,63 +3,88 @@ import { verifyTokens } from './verify-tokens';
 
 describe('XML-like Constructs - Stage 4', () => {
   test('simple CDATA section', () => {
-  const tokenTest = `<![CDATA[test]]>
+  const tokenTest = `
+<![CDATA[test]]>
 1
 @1 HtmlCdata "<![CDATA[test]]>"`;
   expect(verifyTokens(tokenTest)).toBe(tokenTest);
   });
 
   test('simple processing instruction', () => {
-  const tokenTest = `<?xml?>
+  const tokenTest = `
+<?xml?>
 1
 @1 HtmlProcessingInstruction "<?xml?>"`;
   expect(verifyTokens(tokenTest)).toBe(tokenTest);
   });
 
   test('CDATA with simple content', () => {
-  const tokenTest = `<![CDATA[hello world]]>
+  const tokenTest = `
+<![CDATA[hello world]]>
 1
 @1 HtmlCdata "<![CDATA[hello world]]>"`;
   expect(verifyTokens(tokenTest)).toBe(tokenTest);
   });
 
   test('processing instruction with version', () => {
-  const tokenTest = `<?xml version=1.0?>
+  const tokenTest = `
+<?xml version=1.0?>
 1
 @1 HtmlProcessingInstruction "<?xml version=1.0?>"`;
   expect(verifyTokens(tokenTest)).toBe(tokenTest);
   });
 
   test('CDATA with XML-like content', () => {
-  const tokenTest = `<![CDATA[<tag>content</tag>]]>
+  const tokenTest = `
+<![CDATA[<tag>content</tag>]]>
 1
 @1 HtmlCdata "<![CDATA[<tag>content</tag>]]>"`;
   expect(verifyTokens(tokenTest)).toBe(tokenTest);
   });
 
   test('empty CDATA section', () => {
-  const tokenTest = `<![CDATA[]]>
+  const tokenTest = `
+<![CDATA[]]>
 1
 @1 HtmlCdata "<![CDATA[]]>"`;
   expect(verifyTokens(tokenTest)).toBe(tokenTest);
   });
 
   test('processing instruction with target', () => {
-  const tokenTest = `<?target?>
+  const tokenTest = `
+<?target?>
 1
 @1 HtmlProcessingInstruction "<?target?>"`;
   expect(verifyTokens(tokenTest)).toBe(tokenTest);
   });
 
+  test('processing instruction with quoted attributes', () => {
+  const tokenTest = `
+<?xml version="1.0" encoding="UTF-8"?>
+1
+@1 HtmlProcessingInstruction "<?xml version=\\"1.0\\" encoding=\\"UTF-8\\"?>"`;
+  expect(verifyTokens(tokenTest)).toBe(tokenTest);
+  });
+
+  test('processing instruction with single-quoted attribute', () => {
+  const tokenTest = `
+<?target attr='value'?>
+1
+@1 HtmlProcessingInstruction "<?target attr='value'?>"`;
+  expect(verifyTokens(tokenTest)).toBe(tokenTest);
+  });
+
   test('CDATA ignoring XML constructs inside', () => {
-  const tokenTest = `<![CDATA[<!-- comment --> <?pi?>]]>
+  const tokenTest = `
+<![CDATA[<!-- comment --> <?pi?>]]>
 1
 @1 HtmlCdata "<![CDATA[<!-- comment --> <?pi?>]]>"`;
   expect(verifyTokens(tokenTest)).toBe(tokenTest);
   });
 
   test('CDATA with JavaScript-like content', () => {
-  const tokenTest = `<![CDATA[if (x < 5) return;]]>
+  const tokenTest = `
+<![CDATA[if (x < 5) return;]]>
 1
 @1 HtmlCdata "<![CDATA[if (x < 5) return;]]>"`;
   expect(verifyTokens(tokenTest)).toBe(tokenTest);
@@ -69,65 +94,74 @@ describe('XML-like Constructs - Stage 4', () => {
     // This reproduces the original failing pattern where an assertion contains escaped quotes
     // inside a JSON string, e.g. \"<test>\". The verifier must parse the assertion and
     // not enter an infinite loop.
-  const tokenTest = `<![CDATA[ var x = \"<test>\"; ]]>
+  const tokenTest = `
+<![CDATA[ var x = \"<test>\"; ]]>
 1
 @1 HtmlCdata "<![CDATA[ var x = \\\"<test>\\\"; ]]>"`;
   expect(verifyTokens(tokenTest)).toBe(tokenTest);
   });
 
   test('simple escaped quote inside JSON assertion', () => {
-  const tokenTest = `<![CDATA[ var x = \"<test>\"; ]]>
+  const tokenTest = `
+<![CDATA[ var x = \"<test>\"; ]]>
 1
 @1 HtmlCdata "<![CDATA[ var x = \\\"<test>\\\"; ]]>"`;
   expect(verifyTokens(tokenTest)).toBe(tokenTest);
   });
 
   test('double backslash before quote (even number) inside CDATA', () => {
-  const tokenTest = `<![CDATA[ s = "end" ]]>
+  const tokenTest = `
+<![CDATA[ s = "end" ]]>
 1
 @1 HtmlCdata "<![CDATA[ s = \\\"end\\\" ]]>"`;
   expect(verifyTokens(tokenTest)).toBe(tokenTest);
   });
 
   test('triple backslash before quote (odd number) inside CDATA', () => {
-  const tokenTest = `<![CDATA[ s = \"end\" ]]>
+  const tokenTest = `
+<![CDATA[ s = \"end\" ]]>
 1
-@1 HtmlCdata "<![CDATA[ s = \\"end\\" ]]>"`;
+@1 HtmlCdata "<![CDATA[ s = \\\"end\\\" ]]>"`;
   expect(verifyTokens(tokenTest)).toBe(tokenTest);
   });
 
   test('named HTML entity for quote inside CDATA', () => {
-  const tokenTest = `<![CDATA[ &quot;inside&quot; ]]>
+  const tokenTest = `
+<![CDATA[ &quot;inside&quot; ]]>
 1
 @1 HtmlCdata "<![CDATA[ &quot;inside&quot; ]]>"`;
   expect(verifyTokens(tokenTest)).toBe(tokenTest);
   });
 
   test('numeric decimal entity for quote inside CDATA', () => {
-  const tokenTest = `<![CDATA[ &#34;number&#34; ]]>
+  const tokenTest = `
+<![CDATA[ &#34;number&#34; ]]>
 1
 @1 HtmlCdata "<![CDATA[ &#34;number&#34; ]]>"`;
   expect(verifyTokens(tokenTest)).toBe(tokenTest);
   });
 
   test('numeric hex entity for quote inside CDATA', () => {
-  const tokenTest = `<![CDATA[ &#x22;hex&#x22; ]]>
+  const tokenTest = `
+<![CDATA[ &#x22;hex&#x22; ]]>
 1
 @1 HtmlCdata "<![CDATA[ &#x22;hex&#x22; ]]>"`;
   expect(verifyTokens(tokenTest)).toBe(tokenTest);
   });
 
   test('combination: escaped quote plus entity inside CDATA', () => {
-  const tokenTest = `<![CDATA[ mix = \"&quot;\" ]]>
+  const tokenTest = `
+<![CDATA[ mix = \"&quot;\" ]]>
 1
 @1 HtmlCdata "<![CDATA[ mix = \\\"&quot;\\\" ]]>"`;
   expect(verifyTokens(tokenTest)).toBe(tokenTest);
   });
 
   test('unicode escape sequences inside JSON string within CDATA', () => {
-  const tokenTest = `<![CDATA[ u = \"\\u0022\\u003C\\u003E\" ]]>
+  const tokenTest = `
+<![CDATA[u = \"\\u0022\\u003C\\u003E\" ]]>
 1
-@1 HtmlCdata "<![CDATA[ u = \\"\\\\u0022\\\\u003C\\\\u003E\\" ]]>"`;
+@1 HtmlCdata "<![CDATA[u = \\"\\\\u0022\\\\u003C\\\\u003E\\" ]]>"`;
   expect(verifyTokens(tokenTest)).toBe(tokenTest);
   });
 });
