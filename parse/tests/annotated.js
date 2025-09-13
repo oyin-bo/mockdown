3cc1cace1fd52f4d72cd76dd640d76b94c031946
Oleg Mihailik <mihailik@gmail.com>
2025-09-13 13:11:20 +0100

    Prep for the annotated.js harness.

---
+++ parse/tests/annotated.js
@@ -0,0 +1,8 @@
+// @ts-check
+/// <reference types="node" />
+
+// TODO: implement annotated Markdown test harness here
+
+// TODO: 1 - search for all Markdown files in this directory or deeper
+// TODO: 2 - for each file, extract annotations
+// TODO: 3 - for each annotation, run the parser and verify assertions
